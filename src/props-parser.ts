de4c94701434f9392b686faa7f7764b6af9c8255
Ankit Patel <gt3.someday@outlook.com>
2018-11-26 13:06:04 -0800

    allow explicity passing of controlPath, read props from multiple interfaces

--- src/props-parser.ts
+++ src/props-parser.ts
@@ -1,6 +1,7 @@
-import Project from 'ts-simple-ast';
-import { resolveDeclarationPath } from './resolve-declaration';
+import Project, { SourceFile } from 'ts-simple-ast';
+import { checkPathOrThrow, resolveDeclarationPath } from './resolve-declaration';
 import { getComponent } from './parser-utils';
+import signale from 'signale';
 
 export interface PropDoc {
   required: boolean;
@@ -13,28 +14,15 @@ export interface PropDocs {
   [name: string]: PropDoc
 }
 
-// todo: refactor, move out parser specific logic, provide error messages
-export function propsParser(pkg: string, componentName: string): PropDocs {
-  let declarationFile;
-  try {
-    declarationFile = resolveDeclarationPath(pkg);
-  }
-  catch (e) {
-    return null;
-  }
-  const project = new Project({
-    addFilesFromTsConfig: false
-  });
-  const sourceFile = project.addExistingSourceFile(declarationFile);
-  const componentType = getComponent(sourceFile, componentName);
-  const [componentTypeDocs] = componentType.getJsDocs();
-  if (!componentTypeDocs) return null;
-  const propTag = componentTypeDocs.getTags().find(tag => tag.getName() === 'see');
-  if (!propTag) return null;
-  const propsInterfaceName = propTag.getComment().trim();
-  const propsInterface = sourceFile.getInterface(propsInterfaceName);
-  if (!propsInterface) return null;
-  const propsInterfaceProperties = propsInterface.getProperties();
+const getPropsInterfaceProperties = (propsInterfaceNames: string[], sourceFile: SourceFile) => {
+  return propsInterfaceNames.reduce((acc, interfaceName) => {
+    const propsInterface = sourceFile.getInterface(interfaceName);
+    if(propsInterface) acc.push(propsInterface.getProperties());
+    return acc;
+  }, []);
+};
+
+const propsTransform = (propsInterfaceProperties) => {
   return propsInterfaceProperties.reduce((acc, prop) => {
     const propName = prop.getName();
     const required = !prop.getQuestionTokenNode();
@@ -48,4 +36,27 @@ export function propsParser(pkg: string, componentName: string): PropDocs {
       }
     };
   }, {});
+};
+
+// todo: refactor, move out parser specific logic, provide error messages
+export function propsParser(pkg: string, componentName: string, controlPath?: string): PropDocs {
+  let declarationFilePath = controlPath || resolveDeclarationPath(pkg);
+  try {
+    checkPathOrThrow(declarationFilePath);
+  }
+  catch (e) {
+    signale.error(`Declaration file does not exist: ${declarationFilePath}`);
+    return null;
+  }
+  const project = new Project({
+    addFilesFromTsConfig: false
+  });
+  const sourceFile = project.addExistingSourceFile(declarationFilePath);
+  const componentType = getComponent(sourceFile, componentName);
+  const [componentTypeDocs] = componentType.getJsDocs();
+  if (!componentTypeDocs) return null;
+  const propTag = componentTypeDocs.getTags().find(tag => tag.getName() === 'see');
+  if (!propTag) return null;
+  const propsInterfaceNames = propTag.getComment().trim().split(/\s*,\s*/);
+  return propsTransform(getPropsInterfaceProperties(propsInterfaceNames, sourceFile));
 }
--- src/resolve-declaration.ts
+++ src/resolve-declaration.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
+export const checkPathOrThrow = filePath => fs.accessSync(filePath);
+
 export const resolveDeclarationPath = (pkg: string) => {
   const packagePath = require.resolve(pkg);
-  const moduleName = path.basename(packagePath, '.js'), packageDir = path.dirname(packagePath);
-  const declarationPath = path.join(packageDir, `${moduleName}.d.ts`);
-  fs.accessSync(declarationPath);
-  return declarationPath;
+  const moduleName = path.basename(packagePath, '.js'), modulePath = path.dirname(packagePath);
+  return path.join(modulePath, `${moduleName}.d.ts`);
 };
